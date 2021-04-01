0be41ac17d7d286704074ac00c6c89ad90198f0d
zuwang <zuwang12@gmail.com>
2021-04-01 05:29:14 +0000

    test

--- tf/actor_critic_test.py
+++ tf/actor_critic_test.py
@@ -256,6 +256,151 @@ def train_step(
 # + [markdown] id="HFvZiDoAflGK"
 # ### 5. Run the training loop
 
+# +
+def run_episode(
+    initial_state: tf.Tensor,  
+    model: tf.keras.Model, 
+    max_steps: int) -> List[tf.Tensor]:
+    """Runs a single episode to collect training data."""
+
+#     global action_probs, values, rewards, initial_state_shape, state, action_logits_t, test, test1
+
+    action_probs = tf.TensorArray(dtype=tf.float32, size=0, dynamic_size=True)
+    values = tf.TensorArray(dtype=tf.float32, size=0, dynamic_size=True)
+    rewards = tf.TensorArray(dtype=tf.int32, size=0, dynamic_size=True)
+
+    initial_state_shape = initial_state.shape
+    state = initial_state
+    
+    for t in tf.range(max_steps):
+        
+        # Convert state into a batched tensor (batch size = 1)
+        state = tf.expand_dims(state, 0)
+
+        # Run the model and to get action probabilities and critic value
+        action_logits_t, value = model(state)
+        print('t : ', t)
+        print('state : ', state)
+        print('action_logits_t :', action_logits_t)
+        
+        # Sample next action from the action probability distribution
+        action = tf.random.categorical(action_logits_t, 1)[0, 0]
+        action_probs_t = tf.nn.softmax(action_logits_t)
+
+        # Store critic values
+        values = values.write(t, tf.squeeze(value))
+
+        # Store log probability of the action chosen
+        action_probs = action_probs.write(t, action_probs_t[0, action])
+
+        # Apply action to the environment to get next state and reward
+        state, reward, done = tf_env_step(action)
+        state.set_shape(initial_state_shape)
+
+        # Store reward
+        rewards = rewards.write(t, reward)
+
+        if tf.cast(done, tf.bool):
+            break
+            
+    print('before : ', action_probs)
+    action_probs = action_probs.stack()
+    print('after : ', action_probs)
+    values = values.stack()
+    rewards = rewards.stack()
+    
+    return action_probs, values, rewards
+
+
+# + id="QoccrkF3IFCg"
+optimizer = tf.keras.optimizers.Adam(learning_rate=0.01)
+
+
+@tf.function
+def train_step(
+    initial_state: tf.Tensor, 
+    model: tf.keras.Model, 
+    optimizer: tf.keras.optimizers.Optimizer, 
+    gamma: float, 
+    max_steps_per_episode: int) -> tf.Tensor:
+    """Runs a model training step."""
+    global test1, test2, test3
+    
+    print('start tape')
+    with tf.GradientTape() as tape:
+        # Run the model for one episode to collect training data
+        action_probs, values, rewards = run_episode(
+            initial_state, model, max_steps_per_episode)
+        test1, test2, test3 = action_probs, values, rewards
+        print('action_probs : ', action_probs)
+        print('values : ', values)
+        print('rewards : ', rewards)
+        print('run epi')
+
+        # Calculate expected returns
+        returns = get_expected_return(rewards, gamma)
+        print('get return')
+
+        # Convert training data to appropriate TF tensor shapes
+        action_probs, values, returns = [
+            tf.expand_dims(x, 1) for x in [action_probs, values, returns]] 
+        print('expand dim')
+        
+        # Calculating loss values to update our network
+        loss = compute_loss(action_probs, values, returns)
+        print('get loss')
+        
+    print('after tape')
+    # Compute the gradients from the loss
+    grads = tape.gradient(loss, model.trainable_variables)
+
+    # Apply the gradients to the model's parameters
+    optimizer.apply_gradients(zip(grads, model.trainable_variables))
+
+    episode_reward = tf.math.reduce_sum(rewards)
+    print('episode_reward : ', episode_reward)
+    return episode_reward
+
+
+# + id="kbmBxnzLiUJx"
+# %%time
+
+# max_episodes = 10000
+# max_steps_per_episode = 1000
+
+max_episodes = 10
+max_steps_per_episode = 4
+
+# Cartpole-v0 is considered solved if average reward is >= 195 over 100 
+# consecutive trials
+reward_threshold = 195
+running_reward = 0
+
+# Discount factor for future rewards
+gamma = 0.99
+
+with tqdm.trange(max_episodes) as t:
+    for i in t:
+        initial_state = tf.constant(env.reset(), dtype=tf.float32)
+        print('before epi, i: {}'.format(i))
+        episode_reward = int(train_step(
+            initial_state, model, optimizer, gamma, max_steps_per_episode))
+        print('after epi')
+        running_reward = episode_reward*0.01 + running_reward*.99
+
+        t.set_description(f'Episode {i}')
+        t.set_postfix(
+            episode_reward=episode_reward, running_reward=running_reward)
+
+        # Show average episode reward every 10 episodes
+        if i % 10 == 0:
+            pass # print(f'Episode {i}: average reward: {avg_reward}')
+
+        if running_reward > reward_threshold:  
+            break
+
+            print(f'\nSolved at episode {i}: average reward: {running_reward:.2f}!')
+
 # + id="kbmBxnzLiUJx"
 # %%time
 
